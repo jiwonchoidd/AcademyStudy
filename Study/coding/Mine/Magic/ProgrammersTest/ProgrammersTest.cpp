f7fff259d6cbea5cf423a5b0dea8a0788d2508fe
최지원 <popo2022@naver.com>
2021-09-22 23:55:24 +0900

    프로그래머스 문제풀이
    프로그래머스 문제풀이

--- Study/coding/Mine/Magic/ProgrammersTest/ProgrammersTest.cpp
+++ Study/coding/Mine/Magic/ProgrammersTest/ProgrammersTest.cpp
@@ -1,19 +1,33 @@
-﻿#include <iostream>
+﻿#include <string>
 #include <vector>
+#include <iostream>
+
 using namespace std;
 
-int solution(vector<vector<int> > land)
-{
-	int max = 0;
-	int answer = 0;
-
-	for(vector<vector<int>> iterator)
-
-
-	
-	return answer;
+string solution(vector<string> participant, vector<string> completion) {
+    string answer = "";
+    for (int j = 0; j < completion.size(); j++)
+    {
+        for (int i = 0; i < participant.size(); i++)
+        {
+            if (participant[i] == completion[j])
+            {
+                participant.push_back(completion[j]);
+                participant.erase(participant.begin() + i);
+            }
+        }
+    }
+    return participant[0];
 }
-int main(void)
+int main()
 {
-	
+    vector<string> a = {
+        "marina", "josipa", "nikola", "vinko", "filipa"
+    };
+    vector<string> b = {
+        "josipa", "filipa", "marina", "nikola"
+    };
+    solution(a, b);
+
+    cout << "s";
 }
