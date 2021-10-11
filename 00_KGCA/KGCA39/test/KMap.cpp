a0e5bf5a2216f72c134d0f653c10fad966c46cd0
Jiwon <jae2021@gmail.com>
2021-10-11 19:45:04 +0900

    쿼드트리 lod
    쿼드트리 lod

--- 00_KGCA/KGCA39/test/KMap.cpp
+++ 00_KGCA/KGCA39/test/KMap.cpp
@@ -13,6 +13,7 @@ bool	KMap::CreateVertexData()
     // 0  1  2
     // 3  4  5
     // 6  7  8
+   
 
     m_VertexList.resize(m_info.m_iNumVertex);
 
---
+++ Study/최지원LOD/exe/readme.txt
@@ -0,0 +1,2 @@
+1 - fps, timer
+f2 - wireframe
