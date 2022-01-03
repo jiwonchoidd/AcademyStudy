05a23eb6afe69cfa35ca2e3286c7ee04cf51ba22
Jiwon <popo2022@naver.com>
2022-01-03 19:08:32 +0900

    tcp 채팅 클라이언트 서버
    tcp 채팅 클라이언트 서버

---
+++ 01_KGCA/Source/ClientChat/Sample.cpp
@@ -0,0 +1,97 @@
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#define ServerIP "192.168.0.56"
+#include <iostream>
+#include <winsock2.h>
+#include <conio.h>
+#pragma comment	(lib, "ws2_32.lib")
+
+//클라이언트
+void main()
+{
+	WSADATA wsa;
+	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+	{
+		return;
+	}
+	SOCKET sock = socket(AF_INET,
+		SOCK_STREAM, 0); 
+	SOCKADDR_IN sa;
+	ZeroMemory(&sa, sizeof(sa));
+	sa.sin_family = AF_INET;
+	sa.sin_port = htons(10000);
+	sa.sin_addr.s_addr = inet_addr(ServerIP);
+	int iRet = connect(
+		sock,
+		(sockaddr*)&sa,
+		sizeof(sa));
+	if (iRet == SOCKET_ERROR)
+	{
+		return;
+	}
+	std::cout << "접속성공!" << std::endl;
+
+	u_long on = 1;
+	ioctlsocket(sock, FIONBIO, &on);
+
+	char szBuffer[256] = { 0, };
+	int iEnd = 0;
+	while (1)
+	{
+		if (_kbhit() == 1)
+		{
+			int iValue = _getche();
+			if (iValue == '\r' && strlen(szBuffer) == 0)
+			{
+				break;
+			}
+			if (iValue == '\r')
+			{
+				int iSendByte = send(sock,
+					szBuffer,
+					iEnd,
+					0);
+
+				if (iSendByte == SOCKET_ERROR)
+				{
+					if (WSAGetLastError() != WSAEWOULDBLOCK)
+					{
+						std::cout << "비정상 서버종료됨.." << std::endl;
+					}
+				}
+				iEnd = 0;
+				ZeroMemory(szBuffer, sizeof(char) * 256);
+			}
+			else
+			{
+				szBuffer[iEnd++] = iValue;
+			}
+		}
+		else
+		{
+			char szRecvBuffer[256] = { 0, };
+			int iRecvByte = recv(sock, szRecvBuffer, 256, 0);
+
+			if (iRecvByte == 0)
+			{
+				std::cout << "서버종료됨.." << std::endl;
+				break;
+			}
+			if (iRecvByte == SOCKET_ERROR)
+			{
+				if (WSAGetLastError() != WSAEWOULDBLOCK)
+				{
+					std::cout << "비정상 서버종료됨.." << std::endl;
+					break;
+				}
+			}
+			else
+			{
+				std::cout << "\n" << szRecvBuffer;
+				ZeroMemory(szRecvBuffer, sizeof(char) * 256);
+			}
+		}
+	}
+	std::cout << "접속종료" << std::endl;
+	closesocket(sock);
+	WSACleanup();
+}
--- 01_KGCA/Source/Socket_0/Sample.cpp
+++ 01_KGCA/Source/Socket_0/Sample.cpp
@@ -1,39 +1,55 @@
 #define _WINSOCK_DEPRECATED_NO_WARNINGS
-#include "Sample.h"
 #include <iostream>
 #include <winsock2.h>
-#pragma comment (lib, "ws2_32.lib")
-
-//클라이언트 
-int main()
+#pragma comment	(lib, "ws2_32.lib")
+void main()
 {
 	WSADATA wsa;
 	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
 	{
-		return 0;
+		return;
 	}
-	//소캣
-	SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);//IP, STREAM - TCP // DGRAM- UDP, 프로토콜 안넣어도 됨
-
-	//sockaddr addr;
-	//addr.sa_family = AF_INET; //IP 프로토콜 쓴다.
-	//addr.sa_data[14]; // ip. port
-	SOCKADDR_IN sa; //공용체써서 편한 대문자 버젼
+	SOCKET sock = socket(AF_INET,
+		SOCK_STREAM, 0); //SOCK_STREAM, SOCK_DGRAM
+			 //IPPROTO_TCP,IPPROTO_UDP
+	SOCKADDR_IN sa;
 	ZeroMemory(&sa, sizeof(sa));
 	sa.sin_family = AF_INET;
-	sa.sin_port = htons(10000);
-	sa.sin_addr.s_addr = inet_addr("192.118.0.0");
-	int iRet = connect(sock, (sockaddr*)&sa, sizeof(sa)); //연결
+	sa.sin_port = htons(971117);
+	sa.sin_addr.s_addr = inet_addr("192.168.0.56");
+	int iRet = connect(
+		sock,
+		(sockaddr*)&sa,
+		sizeof(sa));
 
-
-	char szBuffer[256] = "Hello World";
-	char szRecvBuffer[256] = { 0, };
-
-	int sendByte = send(sock, szBuffer, sizeof(szBuffer), 0);
-
-	recv(sock, szRecvBuffer, 256, 0);
-
-	std::cout << szBuffer << "\n";
-
+	int iSendCount = 3;
+	while (iSendCount-- > 0)
+	{
+		char szBuffer[] = "안녕하세여.";
+		int iSendByte = send(sock,
+			szBuffer,
+			sizeof(szBuffer),
+			0);
+		if (iSendByte == SOCKET_ERROR)
+		{
+			std::cout << "비정상 서버종료됨.." << std::endl;
+			break;
+		}
+		char szRecvBuffer[256] = { 0, };
+		int iRecvByte = recv(sock, szRecvBuffer, 256, 0);
+		std::cout << szRecvBuffer;
+		if (iRecvByte == 0)
+		{
+			std::cout << "서버종료됨.." << std::endl;
+			break;
+		}
+		if (iRecvByte == SOCKET_ERROR)
+		{
+			std::cout << "비정상 서버종료됨.." << std::endl;
+			break;
+		}
+		Sleep(1000);
+	}
+	closesocket(sock);
 	WSACleanup();
 }
